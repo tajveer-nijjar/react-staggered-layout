55dcc5d15f9c1aa581ae409522c150e99623c6f2
Taj <taj_nijjar2002@yahoo.co.in>
2019-09-12 16:09:05 +1200

    Working good.

--- src/components/TheOtherGrid/index.js
+++ src/components/TheOtherGrid/index.js
@@ -11,18 +11,38 @@ class TheOtherGrid extends React.Component {
     const minHeight = 7;
 
     let layout = [
-      { i: "key1", x: 0, y: 0, w: 4, h: minHeight },
-      { i: "key2", x: 4, y: 0, w: 4, h: minHeight },
-      { i: "key3", x: 8, y: 0, w: 4, h: minHeight }
+      // { i: "key1", x: 0, y: 0, w: 4, h: minHeight + 2, static: true },
+      // { i: "key2", x: 4, y: 0, w: 4, h: minHeight, static: true },
+      // { i: "key3", x: 8, y: 0, w: 4, h: minHeight, static: true }
     ];
 
     for (let i = 0; i < data.length; i++) {
-      const index = `key${i}`;
-      let key = { i: index, x: 0, y: 0, w: 4, h: 14 };
-
+      const index = `key${i + 1}`;
       const descriptionLength = data[i].description.length;
+      const steps = Math.ceil(descriptionLength / 45);
+      const cardHeight = Math.ceil(steps / 2);
 
-      debugger;
+      let xValue;
+      if (i % 3 === 0) {
+        xValue = 0;
+      } else if (i % 3 === 1) {
+        xValue = 4;
+      } else if (i % 3 === 2) {
+        xValue = 8;
+      }
+
+      let yValue = 0;
+
+      let key = {
+        i: index,
+        x: xValue,
+        y: yValue,
+        w: 4,
+        h: minHeight + cardHeight,
+        static: true,
+        index: i
+      };
+
       layout = [...layout, key];
     }
 
@@ -37,18 +57,23 @@ class TheOtherGrid extends React.Component {
           rowHeight={30}
           width={1200}
         >
-          <div className="card" key="key1">
-            {/* abc */}
+          {layout.map(card => {
+            return (
+              <div className="card" key={card.i}>
+                <Card data={data[card.index]} />
+              </div>
+            );
+          })}
+
+          {/* <div className="card" key="key1">
             <Card data={data[0]} />
           </div>
           <div className="card" key="key2">
-            {/* abc */}
             <Card data={data[1]} />
           </div>
           <div className="card" key="key3">
-            {/* abc */}
             <Card data={data[2]} />
-          </div>
+          </div> */}
         </GridLayout>
       </React.Fragment>
     );
